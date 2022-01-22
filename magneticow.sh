25a635f3f8012d6dc0b2af28d4cccec53fb50745
AnimMouse <git@animmouse.com>
2022-01-22 17:22:48 +0800

    Fix unknown URI scheme

--- magneticow.sh
+++ magneticow.sh
@@ -2,4 +2,4 @@
 wget https://github.com/boramalper/magnetico/releases/latest/download/magneticow
 chmod +x magneticow
 wget -O /tmp/database.sqlite3 https://github.com/AnimMouse/magnetico-actions/raw/database/database.sqlite3
-./magneticow -a :$PORT -d /tmp/database.sqlite3 --no-auth
+./magneticow -a :$PORT -d sqlite3:///tmp/database.sqlite3 --no-auth
