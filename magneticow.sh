74a31ac92720a4fd9ae3b0ee541fa30eb1e4af3a
AnimMouse <git@animmouse.com>
2022-01-22 17:19:08 +0800

    Download database

--- magneticow.sh
+++ magneticow.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 wget https://github.com/boramalper/magnetico/releases/latest/download/magneticow
 chmod +x magneticow
-./magneticow -a :$PORT -d https://github.com/AnimMouse/magnetico-actions/raw/database/database.sqlite3 --no-auth
+wget -O /tmp/database.sqlite3 https://github.com/AnimMouse/magnetico-actions/raw/database/database.sqlite3
+./magneticow -a :$PORT -d /tmp/database.sqlite3 --no-auth
