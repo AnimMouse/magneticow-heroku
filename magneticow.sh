09d47173b141b70cdb6f12a816c54fa00ab603b5
AnimMouse <git@animmouse.com>
2022-01-25 00:03:07 +0800

    Pipe to zstd

--- magneticow.sh
+++ magneticow.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-wget -O /tmp/database.sqlite3.zst https://github.com/AnimMouse/magnetico-actions/releases/latest/download/database.sqlite3.zst
-zstd -d /tmp/database.sqlite3.zst
+wget -O - https://github.com/AnimMouse/magnetico-actions/releases/latest/download/database.sqlite3.zst | zstd -d -o /tmp/database.sqlite3
 ./magneticow -a :$PORT -d sqlite3:///tmp/database.sqlite3 --no-auth
