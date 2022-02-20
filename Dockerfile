25c3fa875b6350f70b4a7f8f5cb622937d92184f
AnimMouse <git@animmouse.com>
2022-02-21 00:46:35 +0800

    Use WORKDIR and DATABASE_URL config var

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM frolvlad/alpine-glibc:latest
 RUN apk add --no-cache libstdc++ zstd
+WORKDIR app/
 RUN wget https://github.com/boramalper/magnetico/releases/latest/download/magneticow && chmod +x magneticow
 COPY magneticow.sh .
-CMD ["/magneticow.sh"]
+CMD ["/app/magneticow.sh"]
--- magneticow.sh
+++ magneticow.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-wget -O - https://github.com/AnimMouse/magnetico-actions/releases/latest/download/database.sqlite3.zst | zstd -d -o /tmp/database.sqlite3
+wget -O - $DATABASE_URL | zstd -d -o /tmp/database.sqlite3
 ./magneticow -a :$PORT -d sqlite3:///tmp/database.sqlite3 --no-auth
